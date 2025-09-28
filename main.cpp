5a29a9acc5630a142f9b73e9ba6540252a7347d0
Azmaeen Mahtab <mahtabazmaeen7@gmail.com>
2025-09-28 21:21:01 +0600

    set up successfull:

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    int a ;
     cout << "Hello world!" << endl;
+    cout << "start input a number: " << endl;
+    cin >> a;
+    cout << "You input number is: " << a << endl;
+    cout << "end" << endl;
     return 0;
 }
